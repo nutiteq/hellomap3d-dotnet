732783db3b050ed366c6e2364c20048ac79b5ee0
mtehver <mark.tehver@gmail.com>
2015-02-23 20:52:03 +0200

    Fixes

--- hellomap/MainActivity.cs
+++ hellomap/MainActivity.cs
@@ -59,7 +59,7 @@ namespace HelloMap
 			// Register license
 			Nutiteq.Utils.Log.SetShowError (true);
 			Nutiteq.Utils.Log.SetShowWarn(true);
-			MapView.RegisterLicense("TUN3Q0ZCRGQzR2xweEcxeHl2VmVlTG5LbWhTaVAzMlRBaFJiSlhVUTRzWFdvKzgvTUUwaVhueStFc013eGc9PQoKcHJvZHVjdHM9c2RrLXhhbWFyaW4tYW5kcm9pZC0zLjAuKgpwYWNrYWdlTmFtZT1jb20ubnV0aXRlcS5oZWxsb21hcC54YW1hcmluCndhdGVybWFyaz1udXRpdGVxCg==", ApplicationContext);
+			MapView.RegisterLicense("XTUN3Q0ZCRGQzR2xweEcxeHl2VmVlTG5LbWhTaVAzMlRBaFJiSlhVUTRzWFdvKzgvTUUwaVhueStFc013eGc9PQoKcHJvZHVjdHM9c2RrLXhhbWFyaW4tYW5kcm9pZC0zLjAuKgpwYWNrYWdlTmFtZT1jb20ubnV0aXRlcS5oZWxsb21hcC54YW1hcmluCndhdGVybWFyaz1udXRpdGVxCg==", ApplicationContext);
 
 			// Set our view from the "main" layout resource
 			MapView mapView = new MapView (ApplicationContext);
