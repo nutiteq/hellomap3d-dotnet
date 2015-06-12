4cdb5cd7492dc128e027db5f2ca50b5a2b08ba43
Jaak Laineste <jaak@nutiteq.com>
2015-06-12 18:02:13 +0300

    iOs sample with Clusters and GeoJSON reader. Android not updated yet.

--- hellomap-ios/MainViewController.cs
+++ hellomap-ios/MainViewController.cs
@@ -12,6 +12,9 @@ using Nutiteq.Projections;
 using Nutiteq.Styles;
 using Nutiteq.WrappedCommons;
 using Nutiteq.VectorTiles;
+using Nutiteq.Graphics;
+using Nutiteq.Geometry;
+using Newtonsoft.Json.Linq;
 
 namespace HelloMap
 {
@@ -27,21 +30,31 @@ namespace HelloMap
 
 			// GLKViewController-specific parameters for smoother animations
 			ResumeOnDidBecomeActive = false;
-			PreferredFramesPerSecond = 60;
+			PreferredFramesPerSecond = 45;
 
 			// Register license
 			Nutiteq.Utils.Log.ShowError = true;
 			Nutiteq.Utils.Log.ShowWarn = true;
-			Nutiteq.Ui.MapView.RegisterLicense("XTUMwQ0ZRQzdURnJKck9HYUdhT09VNGFSN3o3Nmg3UWhjQUlVTnV4TStMMk0vemhPMXUwUnBGRlhwbmFtTklFPQoKcHJvZHVjdHM9c2RrLXhhbWFyaW4taW9zLTMuKixzZGsteGFtYXJpbi1hbmRyb2lkLTMuKgpwYWNrYWdlTmFtZT1jb20ubnV0aXRlcS5oZWxsb21hcC54YW1hcmluCmJ1bmRsZUlkZW50aWZpZXI9Y29tLm51dGl0ZXEuaGVsbG9tYXAueGFtYXJpbgp3YXRlcm1hcms9bnV0aXRlcQp2YWxpZFVudGlsPTIwMTUtMDYtMDEKdXNlcktleT0yYTllOWY3NDYyY2VmNDgxYmUyYThjMTI2MWZlNmNiZAo");
+			Nutiteq.Utils.Log.ShowDebug = true;
+			Nutiteq.Ui.MapView.RegisterLicense("XTUM0Q0ZRQytoU2xnd0MrZ2kvV0VhUmlWVGYxK0xsbDJNd0lWQUt6Z245R1F5NW95RXJ5WndoejBNcGpDdGpSZgoKcHJvZHVjdHM9c2RrLXhhbWFyaW4taW9zLTMuKgpidW5kbGVJZGVudGlmaWVyPWNvbS5udXRpdGVxLmhlbGxvbWFwLnhhbWFyaW4Kd2F0ZXJtYXJrPW51dGl0ZXEKdXNlcktleT0yYTllOWY3NDYyY2VmNDgxYmUyYThjMTI2MWZlNmNiZAo=");
 
 			// Create package manager folder (Platform-specific)
 			var paths = NSSearchPath.GetDirectories (NSSearchPathDirectory.ApplicationSupportDirectory, NSSearchPathDomain.User);
 			var packagesDir = paths [0] + "packages";
 			NSFileManager.DefaultManager.CreateDirectory (packagesDir, true, null);
 
+
 			// Initialize map
 			string importPackagePath = AssetUtils.CalculateResourcePath ("world_ntvt_0_4.mbtiles");
 			MapSetup.InitializeMapView (packagesDir, importPackagePath, Map);
+
+			/// Online vector base layer
+//			var baseLayer = new NutiteqOnlineVectorTileLayer("osmbright.zip");
+
+			/// Set online base layer
+//			Map.Layers.Add(baseLayer);
+
+			addJosnLayer ();
 		}
 
 		public override void ViewWillAppear(bool animated) {
@@ -50,6 +63,66 @@ namespace HelloMap
 			// GLKViewController-specific, do on-demand rendering instead of constant redrawing
 			// This is VERY IMPORTANT as it stops battery drain when nothing changes on the screen!
 			Paused = true;
+		}
+
+
+		private void addJosnLayer(){
+			var json = System.IO.File.ReadAllText(AssetUtils.CalculateResourcePath("capitals_3857.geojson"));
+			var features = Newtonsoft.Json.Linq.JObject.Parse (json)["features"];
+
+			var geoJsonParser = new GeoJSONGeometryReader();
+
+			var proj = new EPSG3857 ();
+			var balloonPopupStyleBuilder = new BalloonPopupStyleBuilder();
+
+			// Create overlay layer for markers
+			var dataSource = new LocalVectorDataSource (proj);
+			var overlayLayer = new ClusteredVectorLayer (dataSource, new MyClusterElementBuilder());
+			overlayLayer.MinimumClusterDistance = 80; // in pixels
+			Map.Layers.Add (overlayLayer);
+
+			foreach(var feature in features){
+				var featureType = feature ["type"];
+
+				var geometry = feature ["geometry"];
+				var ntGeom = geoJsonParser.ReadGeometry (Newtonsoft.Json.JsonConvert.SerializeObject(geometry));
+
+				var popup = new BalloonPopup(
+					ntGeom,
+					balloonPopupStyleBuilder.BuildStyle(),
+					(string) feature ["properties"]["Capital"], (string) feature ["properties"]["Country"]);
+
+				var properties = (JObject) feature ["properties"];
+				foreach (var property in properties) {
+					var key = (string) property.Key;
+					var value = (string) property.Value;
+					popup.SetMetaDataElement(key,value);
+				}
+
+				dataSource.Add (popup);
+
+			}
+
+		}
+	}
+
+	class MyClusterElementBuilder : ClusterElementBuilder
+	{
+		BalloonPopupStyleBuilder balloonPopupStyleBuilder;
+
+		public MyClusterElementBuilder(){
+			balloonPopupStyleBuilder = new BalloonPopupStyleBuilder();
+			balloonPopupStyleBuilder.CornerRadius = 3;
+			balloonPopupStyleBuilder.TitleMargins = new BalloonPopupMargins (6, 6, 6, 6);
+			balloonPopupStyleBuilder.LeftColor = new Color(240,230,140,255);
+		}
+
+		public override VectorElement BuildClusterElement(MapPos pos, VectorElementVector elements) {
+			var popup = new BalloonPopup(
+				pos,
+				balloonPopupStyleBuilder.BuildStyle(),
+				elements.Count.ToString(), "");
+			return popup;
 		}
 
 	}
